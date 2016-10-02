3ee24f8f4ffbb49b4d450df274bb35448f7a0615
Channing Walton <channingwalton@mac.com>
2016-10-02 23:00:48 +0100

    add completion to the Typeclasses example

--- src/main/scala/typeclasses/Typeclasses.scala
+++ src/main/scala/typeclasses/Typeclasses.scala
@@ -1,5 +1,7 @@
 package typeclasses
 
+import java.io.Serializable
+
 import model.Address
 
 import scala.xml.NodeSeq
@@ -11,9 +13,9 @@ import scala.xml.NodeSeq
   */
 object Typeclasses {
 
-  type Key = String
-
-  object Basics {
+  // the core model of a question and behaviours
+  object Core {
+    type Key = String
 
     // unsealed trait for question
     trait Question[T] extends Product with Serializable {
@@ -21,10 +23,39 @@ object Typeclasses {
       def question: String
     }
 
-    // A type class for rendering as html
+    // a questionnaire is built with a question and more context
+    final case class QContext[Q <: Question[_]](q: Q)(implicit html: AsHtml[Q], completion: CompletionCalculator[Q]) {
+      def renderHtml: NodeSeq = html.asHtml(q)
+      def completion(answerMap: AnswerMap): Completion = completion.calculate(q, answerMap)
+    }
+
+    case class Answer[T](q: Question[T], v: T)
+
+    type AnswerMap = Map[Key, Answer[_]]
+
     trait AsHtml[A] {
       def asHtml(a: A): NodeSeq
     }
+
+    sealed trait Completion extends Product with Serializable {
+      // the key of the completed / incomplete question
+      def key: Key
+    }
+    case class Complete(key: Key) extends Completion
+    case class NotComplete(key: Key) extends Completion
+
+    trait CompletionCalculator[A] {
+      def calculate(a: A, answerMap: AnswerMap): Completion
+    }
+
+    object CompletionCalculator {
+      def calculate(questions: List[QContext[_]], answerMap: AnswerMap): List[Completion] =
+        questions.map(q ⇒ q.completion(answerMap))
+    }
+  }
+
+  object Basics {
+    import Core._
 
     // questions and companions
     final case class StringQuestion(k: Key, question: String) extends Question[String]
@@ -33,11 +64,13 @@ object Typeclasses {
 
       implicit object StringAsHtml extends AsHtml[StringQuestion] {
         override def asHtml(a: StringQuestion): NodeSeq =
-          <span>
-            {a.question}
-            :</span> ++ <input id={a.k}/>
+          <span>{a.question}:</span> ++ <input id={a.k}/>
       }
 
+      implicit object StringCompletion extends CompletionCalculator[StringQuestion] {
+        override def calculate(q: StringQuestion, answerMap: AnswerMap): Completion =
+          if (answerMap.contains(q.k)) Complete(q.k) else NotComplete(q.k)
+      }
     }
 
     final case class BooleanQuestion(k: Key, question: String) extends Question[Boolean]
@@ -46,30 +79,33 @@ object Typeclasses {
 
       implicit object BooleanAsHtml extends AsHtml[BooleanQuestion] {
         override def asHtml(a: BooleanQuestion): NodeSeq =
-          <span>
-            {a.question}
-            :</span> ++ <checkbox id={a.k}/>
+          <span>{a.question}:</span> ++ <checkbox id={a.k}/>
       }
 
+      implicit object BooleanCompletion extends CompletionCalculator[BooleanQuestion] {
+        override def calculate(q: BooleanQuestion, answerMap: AnswerMap): Completion =
+          if (answerMap.contains(q.k)) Complete(q.k) else NotComplete(q.k)
+      }
     }
   }
 
-  import Basics._
-  // build a questionnaire with instance of the typeclass instances required
-  final case class QContext[Q <: Question[_]](q: Q)(implicit html: AsHtml[Q]) {
-    def renderHtml: NodeSeq = html.asHtml(q)
+  object BasicQuestionnaire {
+    import Core._
+    import Basics._
+
+    val who = StringQuestion("a", "Who are you?")
+    val happy = BooleanQuestion("b", "Happy?")
+    val questionnaire: List[QContext[_]] = QContext(who) :: QContext(happy) :: Nil
+
+    val ui: NodeSeq = questionnaire.map(_.renderHtml).foldLeft(NodeSeq.Empty)(_ ++ _)
+
+    val completions: List[Completion] = CompletionCalculator.calculate(questionnaire, Map("a" → Answer(who, "Channing")))
   }
-
-  val questionnaire: List[QContext[_]] =
-    QContext(StringQuestion("a", "Who are you?")) ::
-      QContext(BooleanQuestion("b", "Happy?")) ::
-      Nil
-
-  val ui: NodeSeq = questionnaire.map(_.renderHtml).foldLeft(NodeSeq.Empty)(_ ++ _)
 
   // now extend it
   object Extensions {
-    import Basics._
+    import Core._
+
     final case class AddressQuestion(k: Key, question: String) extends Question[Address]
 
     object AddressQuestion {
@@ -80,12 +116,20 @@ object Typeclasses {
             {a.question}
             :</span> ++ <input id={s"${a.k}-line1"}/> ++ <input id={s"${a.k}-line2"}/> ++ <input id={s"${a.k}-etc"}/>
       }
-
+      implicit object AddressCompletion extends CompletionCalculator[AddressQuestion] {
+        override def calculate(q: AddressQuestion, answerMap: AnswerMap): Completion =
+          if (answerMap.contains(q.k)) Complete(q.k) else NotComplete(q.k)
+      }
     }
   }
 
-  import Extensions._
-  val extendedQuestionnaire = QContext(AddressQuestion("c", "Where do you live?")) :: questionnaire
+  object ExtendedQuestionnaire {
+    import Core._
+    import BasicQuestionnaire._
+    import Extensions._
 
-  val extendedUI: NodeSeq = extendedQuestionnaire.map(_.renderHtml).foldLeft(NodeSeq.Empty)(_ ++ _)
+    val extendedQuestionnaire = QContext(AddressQuestion("c", "Where do you live?")) :: questionnaire
+
+    val extendedUI: NodeSeq = extendedQuestionnaire.map(_.renderHtml).foldLeft(NodeSeq.Empty)(_ ++ _)
+  }
 }
